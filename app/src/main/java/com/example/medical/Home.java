979bdb2e3778d7801e72701ac5f45c21343176a8
HoThanhHoaiAn <20130193@st.hcmuaf.edu.vn>
2023-05-21 20:11:26 +0700

    update

--- app/src/main/java/com/example/medical/Home.java
+++ app/src/main/java/com/example/medical/Home.java
@@ -6,16 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.medical.R;
 import com.example.medical.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class Home extends Fragment {
 
     private FragmentHomeBinding binding;
 
